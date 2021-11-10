3dc45e71eee53921d6cb420dd2770f72643288b9
Enias Oliveira <eniasoliveira27@gmail.com>
2021-11-10 00:18:33 -0300

    fix: solves all TODOS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,39 +5,41 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
-export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+export const Home = () => {
+	const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+	const handleAddTask = (newTaskTitle: string) => {
+		const randomIntFromTime = new Date().getTime();
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+		const newTask: Task = { id: randomIntFromTime, title: newTaskTitle, done: false };
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+		setTasks((prevTasks) => [...prevTasks, newTask]);
+	};
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+	const handleToggleTaskDone = (id: number) => {
+		const updatedTasks = tasks.map((task) => (task.id === id ? { ...task, done: !task.done } : task));
 
-      <TodoInput addTask={handleAddTask} />
+		setTasks(updatedTasks);
+	};
 
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
-}
+	const handleRemoveTask = (id: number) => {
+		setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+	};
+
+	return (
+		<View style={styles.container}>
+			<Header tasksCounter={tasks.length} />
+
+			<TodoInput addTask={handleAddTask} />
+
+			<TasksList tasks={tasks} toggleTaskDone={handleToggleTaskDone} removeTask={handleRemoveTask} />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+	container: {
+		flex: 1,
+		backgroundColor: '#EBEBEB',
+	},
+});
